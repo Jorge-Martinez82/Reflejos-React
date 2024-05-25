757c9832f72afe690a2acadacd784155fb68eea2
Jorge-Martinez82 <118907308+Jorge-Martinez82@users.noreply.github.com>
2024-05-25 19:41:36 +0200

    Update firebaseConfig.js

--- src/assets/firebaseConfig.js
+++ src/assets/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyATvuZE4j2DUFte-8w76M921nC4EpXDDWc",
-    authDomain: "reflejosest.firebaseapp.com",
-    projectId: "reflejosest",
-    storageBucket: "reflejosest.appspot.com",
-    messagingSenderId: "696976143284",
-    appId: "1:696976143284:web:44c4f404c0d27240b878e1"
+    apiKey: "yourAPIkey",
+    authDomain: "yourauthDomain",
+    projectId: "yourprojectId",
+    storageBucket: "yourstorageBucket",
+    messagingSenderId: "yourmessagingSenderId",
+    appId: "yourAppId"
 };
 
 const app = initializeApp(firebaseConfig);
